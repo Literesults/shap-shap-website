ba623e8b3a1e8889a427d708481d2f1bb5c2e1cd
ebuberoderick <ebuberoderick2@gmail.com>
2023-04-27 08:38:27 +0100

    exported the app

---
+++ out/_next/static/chunks/d7eeaac4-87342fe58c16accb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[617],{8193:function(t,c,n){n.d(c,{ywL:function(){return a}});var u=n(8357);function a(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M872 474H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h720c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"}}]})(t)}}}]);
