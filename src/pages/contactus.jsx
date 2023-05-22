f9777441e4fa5030c93d8e35a475b95a5e3a1395
ebuberoderick <ebuberoderick2@gamil.com>
2023-05-22 15:07:38 +0100

    updating wesite

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -3,14 +3,28 @@ import FormTextArea from '@/components/FormTextArea'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
 import React from 'react'
-import {BiUserCircle} from 'react-icons/bi'
+import { BiUserCircle } from 'react-icons/bi'
 import extraImg from '../../public/images/business-desktop.png'
+import { serialize } from '@/hooks/Serilize'
+import axios from 'axios'
 
 
 function contactus() {
+    const sendFeedBack = (e) => {
+        e.preventDefault()
+        const data = serialize(e.target)
+        console.log(data);
+        const base = 'https://api.hmxexpress.com/api';
+        axios.post(`${base}/support/send-feedback`, data ).then((res) => {
+            console.log(res);
+        }).catch((error) => {
+            console.log(error.response);
+        })
+    }
+
     return (
         <Layout active={'contact us'}>
-            <div> 
+            <div>
                 <div className="pt-24 pb-20  bg-gray-50 dark:bg-gray-900 px-3">
                     <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
                         <div className="flex w-full items-center">
@@ -31,28 +45,28 @@ function contactus() {
                 <div className="p-3 pt-24">
                     <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 mb-28">
                         <div className="my-auto px-8 sm:px-16 md:px-24 lg:px-0 lg:py-12">
-                                <div className="shadow-lg text-white p-5 bg-black dark:bg-gray-800 rounded-lg divide-y relative top-24 lg:top-0 lg:left-32">
-                                    <div className="py-2 cursor-pointer hover:text-gray-300">
-                                        <div className="">E-mail</div>
-                                        <div className="">shapshap@gmail.com</div>
-                                    </div>
-                                    <div className="py-2 cursor-pointer hover:text-gray-300">
-                                        <div className="">Phone</div>
-                                        <div className="">+234 813 0075 358</div>
-                                    </div>
-                                    <div className="py-2 cursor-pointer hover:text-gray-300">
-                                        <div className="">FaceBook</div>
-                                        <div className="">shapshap@gmail.com</div>
-                                    </div>
-                                    <div className="py-2 cursor-pointer hover:text-gray-300">
-                                        <div className="">Instagram</div>
-                                        <div className="">shapshap@gmail.com</div>
-                                    </div>
-                                    <div className="py-2 cursor-pointer hover:text-gray-300">
-                                        <div className="">Twitter</div>
-                                        <div className="">shapshap@gmail.com</div>
-                                    </div>
+                            <div className="shadow-lg text-white p-5 bg-black dark:bg-gray-800 rounded-lg divide-y relative top-24 lg:top-0 lg:left-32">
+                                <div className="py-2 cursor-pointer hover:text-gray-300">
+                                    <div className="">E-mail</div>
+                                    <div className="">shapshap@gmail.com</div>
                                 </div>
+                                <div className="py-2 cursor-pointer hover:text-gray-300">
+                                    <div className="">Phone</div>
+                                    <div className="">+234 813 0075 358</div>
+                                </div>
+                                <div className="py-2 cursor-pointer hover:text-gray-300">
+                                    <div className="">FaceBook</div>
+                                    <div className="">shapshap@gmail.com</div>
+                                </div>
+                                <div className="py-2 cursor-pointer hover:text-gray-300">
+                                    <div className="">Instagram</div>
+                                    <div className="">shapshap@gmail.com</div>
+                                </div>
+                                <div className="py-2 cursor-pointer hover:text-gray-300">
+                                    <div className="">Twitter</div>
+                                    <div className="">shapshap@gmail.com</div>
+                                </div>
+                            </div>
                         </div>
                         <div className="md:col-span-2 flex items-center bg-gray-100 dark:bg-gray-600 rounded-3xl px-5 pb-5 pt-28 lg:pl-36 lg:py-5 lg:pr-5">
                             <div className="space-y-7 w-full">
@@ -62,10 +76,11 @@ function contactus() {
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et amet enim ipsa adipisci, consequuntur sunt deleniti nesciunt quibusdam temporibus laborum ipsam rerum earum ea quia facilis nobis vitae soluta!
                                     </div>
                                 </div>
-                                <form className="px-3 md:px-16 py-8 space-y-3">
-                                    <FormInput Icon={<BiUserCircle size={'24px'}/>} name={'fullname'} placeholder={'Enter Fullname'} type={'text'} />
-                                    <FormInput Icon={<BiUserCircle size={'24px'}/>} name={'contact'} placeholder={'Enter Phone or Mail'} type={'text'} />
-                                    <FormTextArea Icon={<BiUserCircle size={'24px'}/>} name={'message'} placeholder={'Write Your Message here'} />
+                                <form onSubmit={sendFeedBack} className="px-3 md:px-16 py-8 space-y-3">
+                                    {/* <input type="hidden" name="contact" value="" /> */}
+                                    <FormInput Icon={<BiUserCircle size={'24px'} />} name={'name'} placeholder={'Enter Fullname'} type={'text'} />
+                                    <FormInput Icon={<BiUserCircle size={'24px'} />} name={'contact_info'} placeholder={'Enter Phone or Mail'} type={'text'} />
+                                    <FormTextArea Icon={<BiUserCircle size={'24px'} />} name={'message'} placeholder={'Write Your Message here'} />
                                     <button type="submit" className="bg-gray-900 w-full rounded-lg text-white text-center py-2 uppercase text-lg cursor-pointer hover:bg-black">
                                         drop message
                                     </button>
