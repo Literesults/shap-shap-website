888731c9ca89cfc586df045603f59564c155e810
ebuberoderick <ebuberoderick2@gmail.com>
2023-06-08 19:24:23 +0100

    fixed Notification Bugs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navigation from './Navigation'
 import Footer from './Footer'
 import ContactCard from './ContactCard'
 import Notification from 'react-toast-notifier';
+import { useRouter } from 'next/router'
 
 function Layout({ children, active }) {
+    const router = useRouter()
+    const [ready,setReady] = useState(false)
+    useEffect(() => {
+        setReady(true)
+    }, [router.query.name, router.isReady])
+
     return (
-        <div onScroll={() => handleScroll()} className="overflow-x-hidden">
-            <Notification />
+        <div className="overflow-x-hidden">
+            {ready && <Notification />}
             <Navigation active={active} />
             <div className="min-h-screen">{children}</div>
             <ContactCard />
