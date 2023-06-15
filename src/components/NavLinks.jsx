c979b5f82c870d2e7fcdbc4c8d314fa32de33ad1
ebuberoderick <ebuberoderick2@gmail.com>
2023-06-15 07:36:08 +0100

    adjusted the navigstion

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function NavLinks({title,active,route}) {
+  useEffect(() => {
+    AOS.init();
+  }, [])
+  
   return (
-    <Link href={route} className={`px-2 group capitalize py-2 md:py-0 cursor-pointer dark:hover:text-white hover:text-black overflow-hidden ${active.toLowerCase() == title.toLowerCase() ? 'text-yellow-300' : ' text-black dark:text-white' } `}>
+    <Link  data-aos="fade-up" href={route} className={`px-2 group capitalize py-2 md:py-0 cursor-pointer dark:hover:text-white hover:text-black overflow-hidden ${active.toLowerCase() == title.toLowerCase() ? 'text-yellow-300' : ' text-black dark:text-white' } `}>
         {active !== title}
         <div>{title}</div>
         <div className={`h-[1.8px] relative group-hover:top-0 bg-black dark:bg-white md:w-2/3 mx-auto ${active.toLowerCase() != title.toLowerCase() && 'top-4' }`}/>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -51,21 +51,22 @@ function Navigation({ active }) {
         </div>
         <div className={`flex-grow fixed md:relative justify-center h-screen md:h-auto transition-all duration-300 w-screen bg-opacity-30 backdrop-blur-md md:backdrop-blur-0 dark:backdrop-blur-md dark:bg-opacity-30 dark:md:backdrop-blur-0 bg-gray-100 dark:bg-gray-800 dark:md:bg-transparent md:bg-transparent top-0 left-0  ${menu ? '' : 'hidden md:flex'} flex-col md:flex-row`}>
           <div className="w-full md:p-0">
-            <div className="md:flex mx-auto bg-white md:bg-transparent dark:bg-gray-900 dark:md:bg-transparent">
-              <div className="flex-grow flex-col md:flex-row relative flex justify-center pt-10 md:pt-0">
+            <div className="md:flexmx-auto h-screen md:h-auto bg-white md:bg-transparent dark:bg-gray-900 dark:md:bg-transparent">
+              <div className="flex-grow flex-col md:flex-row relative flex gap-6 justify-center pt-10 md:pt-0">
                 <Image src={logoLight} alt='' width={'80'} height={'54'} className="rounded-lg absolute top-3 left-2  hidden dark:block md:dark:hidden cursor-pointer" draggable="false" />
                 <Image src={Logodark} alt='' width={'80'} height={'54'} className="rounded-lg absolute top-3 left-2  dark:hidden md:hidden cursor-pointer" draggable="false" />
+                
                 <NavLinks route="/" title="Home" active={active} />
-
                 <NavLinks route="/about" title="About Us" active={active} />
                 {/* <NavLinks route="/about" title="About" active={active} /> */}
                 <NavLinks route="/contactus" title="Contact us" active={active} />
                 {/* <NavLinks route="/terms-conditions" title="Terms-Conditions" active={active} /> */}
                 <NavLinks route="/faq" title="Faq" active={active} />
                 <NavLinks route="/blogs" title="Blog" active={active} />
+                <div className="relative w-full bottom-1 p-2"><RiderBtn /></div>
               </div>
               <div className="flex py-12 md:py-0 px-4 relative md:px-0 gap-3 items-center">
-                <div className="hidden md:block absolute right-14"><RiderBtn /></div>
+                <div className="hidden md:block absolute right-14 w-40"><RiderBtn /></div>
                 {/* <div>extra</div> */}
                 <ThemeToggler isDark={() => setIsDark(!isDark)} />
               </div>
@@ -73,11 +74,10 @@ function Navigation({ active }) {
           </div>
         </div>
         <div className="md:hidden flex gap-3 items-center">
-          <div className="relative bottom-1"><RiderBtn /></div>
           <div onClick={() => toggleNow()} className="space-y-2 w-8 h-8 relative bottom-1 left-1 md:hidden rounded-md flex flex-col items-center justify-center cursor-pointer">
             <div className={`h-[2px] bg-gray-600 dark:bg-gray-200 transition-all duration-700 ${menu ? 'w-full transform rotate-45 absolute' : 'w-2/3'}`}></div>
             <div className={`h-[2px] bg-gray-600 dark:bg-gray-200 relative transform transition-all duration-700 ${menu ? 'rotate-180 w-0' : 'w-full'}`}></div>
-            <div className={`h-[2px] bg-gray-600 dark:bg-gray-200 transition-all duration-700 ${menu ? 'w-full transform -rotate-45 absolute' : 'w-2/3'}`}></div>
+            <div className={`h-[2px] bg-gray-600 dark:bg-gray-200 transition-all duration-700 ${menu ? 'w-full transform -rotate-45 absolute top-2' : 'w-2/3'}`}></div>
           </div>
         </div>
       </div>
