b75584b843f746816e6a3f45bcf7c5a351aeafc3
ebuberoderick <ebuberoderick2@gmail.com>
2023-06-17 18:04:20 +0100

    added policy content

--- src/pages/terms-conditions.jsx
+++ src/pages/terms-conditions.jsx
@@ -80,9 +80,57 @@ function Termsconditions() {
     }
   ]
 
-  const policy = []
+  const policies = [
+    {
+      title: 'Information Collection',
+      description: [
+        'Personal Identification Information: We may collect personal identification information from users in various ways, including when users register an account, book a ride, or interact with the App. The information we may collect includes but is not limited to name, email address, phone number, and payment details.',
+        'Non-Personal Identification Information: We may collect non-personal identification information about users whenever they interact with the App. This information may include the type of device used, the operating system, the browser used, and other technical information.',
+      ]
+    },
+    {
+      title: 'Information Usage',
+      description: [
+        'To Provide Services: We may use the information collected from users to provide transportation services, process payments, and communicate with users regarding their bookings and account.',
+        'To Improve User Experience: We may use the information to understand how users interact with the App, analyze usage patterns, and improve the functionality and user experience of the App.',
+        "To Personalize User Experience: We may use the information to customize and personalize the content and offerings within the App based on users' preferences.",
+        "To Send Periodic Emails: We may use the email address provided by users to send them information and updates pertaining to their bookings, as well as promotional content and company news. Users may opt-out of receiving such emails at any time."
+      ]
+    },
+    {
+      title: 'Information Protection',
+      description: [
+        'We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of personal information and data stored in the App.',
+      ]
+    },
+    {
+      title: 'Information Disclosure',
+      description: [
+        'Service Providers: We may engage third-party service providers to assist us in operating the App and delivering services to users. These service providers will have access to the necessary information to perform their tasks but are obligated not to disclose or use it for any other purpose.',
+        'Legal Compliance: We may disclose personal information if required to do so by law or in response to valid legal requests, such as a court order or government investigation.',
+      ]
+    },
+    {
+      title: "Children's Privacy",
+      description: [
+        'The App is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children. If we discover that we have inadvertently collected information from a child, we will promptly delete it.',
+      ]
+    },
+    {
+      title: 'Changes to this Privacy Policy',
+      description: [
+        'Shap Cab reserves the right to update or change this Privacy Policy at any time. Users are encouraged to review this Privacy Policy periodically to stay informed about how we are protecting and using their information. The updated Privacy Policy will be effective immediately upon posting on the App.',
+      ]
+    },
+    {
+      title: 'Your Acceptance of these Terms',
+      description: [
+        'By using the Shap Cab App, you signify your acceptance of this Privacy Policy. If you do not agree to this Privacy Policy, please do not use the App.',
+      ]
+    }
+  ]
 
-  
+
   return (
     <Layout active={'terms-conditions'}>
       <div className="bgImg">
@@ -127,7 +175,27 @@ function Termsconditions() {
         <div id="policy" className="space-y-3 pt-12">
           <div className="text-lg font-bold dark:text-white">Policy</div>
           <div className="space-y-5 text-sm md:text-base">
-
+            <p>Effective Date: [Insert Date]</p>
+            <p>This Privacy Policy governs the manner in which Shap Cab collects, uses, maintains, and discloses information collected from users (referred to as "you" or "users") of the Shap Cab mobile application ("App"). This Privacy Policy applies to the App and all products and services offered by Sharp Ride.</p>
+            <div className="space-y-3">
+              {
+                policies.map((policy, index) => (
+                  <div className="" key={policy[index]}>
+                    <div className="font-bold">{index + 1}. {policy.title}</div>
+                    <div className="pl-3">
+                      {
+                        policy.description.map((description, inx) => (
+                          <div className="" key={inx}> {
+                            policy.description.length > 1 && <div className="">{index + 1}.{inx + 1}</div>
+                          } {description}</div>
+                        ))
+                      }
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+            <p>If you have any questions about this Privacy Policy, please contact us at [insert contact information].</p>
           </div>
         </div>
       </div>
